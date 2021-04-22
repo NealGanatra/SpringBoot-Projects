4ede722654d2baa92ad2ffc18f944a48354aa121
Neel <neelg817@gmail.com>
2021-04-22 18:24:52 +0530

    First Commit for Email API Backend using spring boot

---
+++ emailApi/src/main/java/com/email/model/EmailResponse.java
@@ -0,0 +1,26 @@
+package com.email.model;
+
+public class EmailResponse {
+	private String token;
+
+	public EmailResponse(String token) {
+		super();
+		this.token = token;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public EmailResponse() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
